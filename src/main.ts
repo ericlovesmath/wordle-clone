f81cf675f1ab3f7174ec2d244b5e1ef2543b91c5
Eric Lee <ericlovesmath@gmail.com>
2022-03-01 10:54:04 -0800

    Fix gh-pages, non-responsive wordle grid layout

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,18 @@ import './style.css'
 
 const $gameContainer = document.querySelector<HTMLDivElement>('#board-container')!;
 
-let tile = document.createElement('div');
-tile.classList.add('tile');
-$gameContainer.appendChild(tile);
+let game_rows: HTMLDivElement[] = [];
+for (let i = 0; i < 6; i++) {
+  let row = document.createElement('div');
+  row.classList.add('game-row');
+  for (let j = 0; j < 5; j++) {
+    let tile = document.createElement('div');
+    tile.classList.add('tile');
+    row.appendChild(tile);
+    $gameContainer.appendChild(row);
+  }
+  game_rows.push(row);
+  console.log("ahh");
+}
+console.log("done");
+
