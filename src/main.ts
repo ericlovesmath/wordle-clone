bc9b0b7d5def142eb205e11968767a252ad070c2
Eric Lee <ericlovesmath@gmail.com>
2022-03-01 17:47:38 -0800

    Keyboard + Grid template

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,60 @@
 import './style.css'
 
-const $gameContainer = document.querySelector<HTMLDivElement>('#board-container')!;
+const $gameBoard = document.querySelector<HTMLDivElement>('#game-board')!;
+const $keyboard = document.querySelector<HTMLDivElement>('#keyboard')!;
 
-let game_rows: HTMLDivElement[] = [];
-for (let i = 0; i < 6; i++) {
-  let row = document.createElement('div');
-  row.classList.add('game-row');
-  for (let j = 0; j < 5; j++) {
-    let tile = document.createElement('div');
-    tile.classList.add('tile');
-    row.appendChild(tile);
-    $gameContainer.appendChild(row);
+// Guess Grid
+
+let GUESS_LEN = 5;
+let GUESS_LIMIT = 6;
+
+initGrid(GUESS_LEN, GUESS_LIMIT);
+initKeyboard();
+
+function initGrid(guess_len: number, guess_limit: number): HTMLDivElement[] {
+  let game_rows: HTMLDivElement[] = [];
+  for (let i = 0; i < guess_limit; i++) {
+    let row = document.createElement('div');
+    row.classList.add('game-row');
+    for (let j = 0; j < guess_len; j++) {
+      let tile = document.createElement('div');
+      tile.classList.add('tile');
+      row.appendChild(tile);
+      $gameBoard.appendChild(row);
+    }
+    game_rows.push(row);
   }
-  game_rows.push(row);
-  console.log("ahh");
+  return game_rows;
 }
-console.log("done");
 
+function initKeyboard() {
+  let key: HTMLDivElement;
+
+  for (let i = 0; i < 10; i++) {
+    key = document.createElement('div');
+    key.classList.add('key');
+    $keyboard.children[0].appendChild(key);
+  }
+
+  for (let i = 0; i < 9; i++) {
+    key = document.createElement('div');
+    key.classList.add('key');
+    $keyboard.children[1].appendChild(key);
+  }
+
+  key = document.createElement('div');
+  key.classList.add('key');
+  key.classList.add('wide-key');
+  $keyboard.children[2].appendChild(key);
+
+  for (let i = 0; i < 7; i++) {
+    key = document.createElement('div');
+    key.classList.add('key');
+    $keyboard.children[2].appendChild(key);
+  }
+
+  key = document.createElement('div');
+  key.classList.add('key');
+  key.classList.add('wide-key');
+  $keyboard.children[2].appendChild(key);
+}
